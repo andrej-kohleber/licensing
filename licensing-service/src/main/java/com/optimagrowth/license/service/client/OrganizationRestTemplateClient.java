0f3ee38e400bf23e5ed275552ef85afdfa0dd3a2
Andrey Koleber <andrei.koleber@gmail.com>
2023-01-05 22:49:35 +0600

    added keycloak and spring-security

--- licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -1,10 +1,10 @@
 package com.optimagrowth.license.service.client;
 
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import com.optimagrowth.license.model.Organization;
 
@@ -12,12 +12,12 @@ import com.optimagrowth.license.model.Organization;
 public class OrganizationRestTemplateClient {
     
     @Autowired
-    RestTemplate restTemplate;
+    KeycloakRestTemplate restTemplate;
 
     public Organization getOrganization(String organizationId){
         ResponseEntity<Organization> restExchange =
                 restTemplate.exchange(
-                        "http://organization-service/v1/organization/{organizationId}",
+                        "http://gateway:8072/organization/v1/organization/{organizationId}",
                         HttpMethod.GET,
                         null, Organization.class, organizationId);
 
